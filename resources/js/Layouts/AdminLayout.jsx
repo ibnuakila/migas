bb706370271cda04d2ec046d14b9237ab6a0197f
Ibnu Akil <ibnuakilofficial@gmail.com>
2023-08-26 22:02:19 +0700

    Update

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,16 +1,14 @@
 import Sidebar from './Sidebar';
 import Header from './Header';
 
-export default function AdminLayout( {auth, content}){
+export default function AdminLayout( {props, content}){
     return (
-            <div class="flex flex-row h-screen w-screen overflow-hidden">
-                <Sidebar />
-                <div className="flex-1">
-                    <Header />
-                    <div className="">{content}</div>
+            <div className="flex flex-wrap h-screen w-screen">                                
+                <div className="w-screen h-screen">
+                    
+                    <Header auth={props.auth}/>
+                    {content}
                 </div>
-            
-            
             </div>
             );
 
