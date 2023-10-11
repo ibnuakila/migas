5ab250e49d39a6f9b1a9d7dbfb60d1060928eb7c
Ibnu Akil <ibnuakilofficial@gmail.com>
2023-10-11 22:40:57 +0700

    Update

--- app/Http/Controllers/InputRealisasiController.php
+++ app/Http/Controllers/InputRealisasiController.php
@@ -10,13 +10,18 @@ use App\Models\InputRealisasi;
 use App\Http\Requests\InputRealisasiRequest;
 use App\Http\Resources\InputRealisasiResource;
 use App\Http\Resources\InputRealisasiCollection;
+use Illuminate\Support\Facades\DB;
 
 class InputRealisasiController extends Controller //implements ICrud
 {
     //
     public function create() {
-        //return Inertia::render('InputRealisasi/FormRealisasi');
-        return "Hello world";
+        return Inertia::render('InputRealisasi/FormRealisasi',[
+            'triwulan' => \App\Models\Triwulan::all(),
+            'periode' => \App\Models\Periode::all(),
+            'pics' => \App\Models\PIC::all()
+        ]);
+        
     }
 
     public function destroy() {
@@ -31,13 +36,18 @@ class InputRealisasiController extends Controller //implements ICrud
 
     public function index(\App\Models\LaporanCapaian $laporancapaian) {
         return Inertia::render('InputRealisasi/ListInputRealisasi',[
+            'indikator' => DB::table('laporan_capaian')
+                ->join('indikator_periode', 'laporan_capaian.indikator_periode_id', '=', 'indikator_periode.id')
+                ->join('indikator', 'indikator_periode.indikator_id', '=', 'indikator.id')
+                ->where('laporan_capaian.id', $laporancapaian->id)->get(),
+                
             'input_realisasis' => InputRealisasi::query()
                 ->join('indikator_kompositor', 'input_realisasi.indikator_kompositor_id', '=', 'indikator_kompositor.id')
-            ->join('indikator', 'indikator_kompositor.indikator_id', '=', 'indikator.id')
-            ->join('indikator_periode', 'indikator.id', '=', 'indikator_periode.indikator_id')
-            ->join('laporan_capaian','indikator_periode.id', '=', 'laporan_capaian.indikator_periode_id')
-            ->where('laporan_capaian.id',$laporancapaian->id)
-            ->get()
+                ->join('indikator', 'indikator_kompositor.indikator_id', '=', 'indikator.id')
+                ->join('indikator_periode', 'indikator.id', '=', 'indikator_periode.indikator_id')
+                ->join('laporan_capaian','indikator_periode.id', '=', 'laporan_capaian.indikator_periode_id')
+                ->where('laporan_capaian.id',$laporancapaian->id)
+                ->get()
         ]);
     }
 
