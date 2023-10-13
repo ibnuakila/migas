59e84fc510a49d97eeb44dacb8fe5d4446cb9179
Ibnu Akil <ibnuakilofficial@gmail.com>
2023-10-13 10:54:33 +0700

    Update

--- resources/js/Pages/InputRealisasi/ListInputRealisasi.jsx
+++ resources/js/Pages/InputRealisasi/ListInputRealisasi.jsx
@@ -15,7 +15,7 @@ export default function ListInputRealisasi({auth}){
     const TABLE_HEAD = ["ID", "Nama Kompositor", "Realisasi", "Satuan", "Triwulan", "PIC", "Periode", "Action"];
  
     const { input_realisasis, indikator } = usePage().props;
-    console.log(indikator[0].nama_indikator);
+    console.log(usePage().props);
     const [open, setOpen] = useState(false);
  
     
@@ -45,7 +45,7 @@ export default function ListInputRealisasi({auth}){
     
     function handleImport(){
         if (confirm('Apakah Anda yakin akan mengimport data indikator?')) {
-            router.visit('/input-realisasi/importkompositor', {
+            router.visit('/input-realisasi/import-kompositor/', {
                 method: 'get',
                 data:{isImport:true},
                 onFinish: visit => {
@@ -62,7 +62,7 @@ export default function ListInputRealisasi({auth}){
                 <div className="container mx-auto">
                     <Card className="p-5 h-full w-full overflow-scroll">
                     <div className="flex justify-between">
-                        <Typography variant="h4">Input Realisasi Kompositor/Parameter {indikator[0].nama_indikator}                           
+                        <Typography variant="h4">Input Realisasi Kompositor/Parameter {indikator.nama_indikator}                     
                         </Typography>
                         <span><Input variant="outlined" size="md" className="w-45" label="Search for Indikator" name="namaIndikator" onChange={handleChange}/></span>
                     </div>
@@ -90,7 +90,55 @@ export default function ListInputRealisasi({auth}){
                                 </tr>
                             </thead>
                             <tbody>                                                      
-                                
+                                {input_realisasis.map(({ id, nama_kompositor, realisasi, satuan, triwulan, pic_id, periode}) => (
+                                    <tr key={id} className="even:bg-blue-gray-50/50">
+                                      <td className="p-4">
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                          {id}
+                                        </Typography>
+                                      </td>
+                                      <td className="p-4">                                      
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-gray-600">
+                                          {nama_kompositor}
+                                        </Typography>                                                                                
+                                      </td>
+                                      <td className="p-4">                                      
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-green-600">
+                                          {realisasi}
+                                        </Typography>                                                                                
+                                      </td>
+                                      <td className="p-4">                                      
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-green-600">
+                                          {satuan}
+                                        </Typography>                                                                                
+                                      </td>
+                                      <td className="p-4">
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-blue-600">
+                                          {triwulan}
+                                        </Typography>
+                                      </td>
+                                      <td className="p-4">                                      
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-gray-600">
+                                          {pic_id}
+                                        </Typography>                                                                                
+                                      </td>                                      
+                                      <td className="p-4">                                      
+                                        <Typography variant="small" color="blue-gray" className="font-normal text-gray-600">
+                                          {periode}
+                                        </Typography>                                                                                
+                                      </td>
+                                      <td className="flex mt-2">
+                                        <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                                          <Link href={route('hitung-kompositor.edit', id)} title="Edit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                              <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                          </Link>
+                                        </Typography>
+                                        
+                                      </td>
+                                    </tr>
+                                  ))}
                             </tbody>
                         </table>
                         <Pagination links={input_realisasis.links} />
