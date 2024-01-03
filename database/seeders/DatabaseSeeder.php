a1a61a9fe28236c25024ce6547e95dde4ac56ed8
Ibnu Akil <ibnuakilofficial@gmail.com>
2024-01-03 22:24:34 +0700

    Update sickai-migas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,70 +12,79 @@ class DatabaseSeeder extends Seeder
 {
     
      private $permissions = [
+            //'user-list',
+            'user-create',
+            'user-edit',
+            'user-delete',
+         
             'profile-edit',
             'profile-update',
             'profile-delete',
+         
             'periode-list',
             'periode-edit',
-            'periode-create',
-            'periode-store',
-            'periode-update',
+            'periode-create',            
             'periode-delete',
+         
             'indikator-list',
-            'indikator-create',
-            'indikator-store',
-            'indikator-edit',
-            'indikator-update',
+            'indikator-create',            
+            'indikator-edit',            
             'indikator-delete',
-            'indikator-kompositor-list',
-            'indikator-kompositor-create',
-            'indikator-kompositor-store',
-            'indikator-kompositor-edit',
-            'indikator-kompositor-update',
-            'indikator-kompositor-delete',
-            'indikator-kompositor-list-indikator',
+         
+            'kompositor-list',
+            'kompositor-create',            
+            'kompositor-edit',            
+            'kompositor-delete',
+            'kompositor-list-of-indikator',
+         
             'hitung-kompositor-list',
-            'hitung-kompositor-create',
-            'hitung-kompositor-store',
-            'hitung-kompositor-edit',
-            'hitung-kompositor-update',
+            'hitung-kompositor-create',            
+            'hitung-kompositor-edit',            
             'hitung-kompositor-delete',
+         
             'pic-list',
             'pic-create',
             'pic-edit',
             'pic-delete',
+         
             'laporan-capaian-list',
             'laporan-capaian-create',
             'laporan-capaian-edit',
             'laporan-capaian-delete',
             'laporan-capaian-importindikator',
-            'laporan-capaian-calculatekinerja',
+                     
             'input-realisasi-list',
             'input-realisasi-create',
             'input-realisasi-edit',
             'input-realisasi-delete',
             'input-realisasi-importkompositor',
             'input-realisasi-calculaterealization',
+         
             'satuan-list',
             'satuan-create',
             'satuan-edit',
             'satuan-delete',
+         
             'level-list',
             'level-create',
             'level-edit',
             'level-delete',
+         
             'indeks-list',
             'indeks-create',
             'indeks-edit',
             'indeks-delete',
+         
             'kategori-kinerja-list',
             'kategori-kinerja-create',
             'kategori-kinerja-edit',
             'kategori-kinerja-delete',
+         
             'input-kinerja-list',
             'input-kinerja-create',
             'input-kinerja-edit',
-            'input-kinerja-delete'
+            'input-kinerja-delete',
+            'input-kinerja-calculate-kinerja'
         ];
     /**
      * Seed the application's database.
@@ -91,19 +100,19 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         foreach ($this->permissions as $permission) {
-            //Permission::create(['name' => $permission]);
+            Permission::create(['name' => $permission]);
         }
         
         // Create admin User and assign the role to him.
-        /*$user = \App\Models\User::create([
+        $user = \App\Models\User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('P@ssw0rd'),
             'pic_id' => 1,
-        ]);*/
+        ]);
         $user = \App\Models\User::where('id', 1)->first();
         
-        //$role = Role::create(['name' => 'Administrator']);
+        $role = Role::create(['name' => 'Administrator']);
         $role = Role::where('name', 'Administrator')->first();
         
         $permissions = Permission::pluck('id', 'id')->all();
