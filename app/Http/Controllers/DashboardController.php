5bf9c699e106da9835229e014396594a13a4930d
Ibnu Akil <ibnu.ial@bsi.ac.id>
2025-04-29 23:43:16 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,51 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Periode;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
+        //data periode
+
+
+        $chartsConfig = [
+            // Add your chartsConfig values here
+        ];
+
+        $chartsConfigXaxis = [
+            // Add your chartsConfig['xaxis'] values here
+        ];
+
+        $websiteViewsChart = [
+            "type" => "bar",
+            "height" => 220,
+            "series" => [
+                [
+                    "name" => "Views",
+                    "data" => [50, 20, 10, 22, 50, 10, 40],
+                ],
+            ],
+            "options" => array_merge(
+                $chartsConfig,
+                [
+                    "colors" => "#388e3c",
+                    "plotOptions" => [
+                        "bar" => [
+                            "columnWidth" => "16%",
+                            "borderRadius" => 5,
+                        ],
+                    ],
+                    "xaxis" => array_merge(
+                        $chartsConfigXaxis,
+                        [
+                            "categories" => ["M", "T", "W", "T", "F", "S", "S"],
+                        ]
+                    ),
+                ]
+            ),
+        ];
+
+        // Output as JSON if needed
+        //$websiteViewsChartJson = json_encode($websiteViewsChart, JSON_PRETTY_PRINT);
+
+        $rerata_capaian_kinerja = DB::table('pic')
+            ->join('laporan_capaian_pic', 'pic.id', '=', 'laporan_capaian_pic.pic_id')
+            ->join('laporan_capaian', 'laporan_capaian_pic.laporan_capaian_id', '=', 'laporan_capaian.id')
+            ->select('pic.nama_pic', DB::raw('AVG(laporan_capaian.kinerja_tahunan) AS rerata_kinerja'))
+            ->where('parent_id', 1)
+            ->groupBy('pic.nama_pic')
+            ->get();
+
         return Inertia::render('Dashboard/home', [
             'periode' => \App\Models\Periode::where('status', 'Active')->first(),
             'user_count' => \App\Models\User::all()->count(),
             'indikator_count' => \App\Models\Indikator::all()->count(),
             'pic_count' => \App\Models\PIC::all()->count(),
-            'indeks_migas' => function(){
+            'indeks_migas' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
-            'indeks_aksesibilitas' => function(){
+            'indeks_aksesibilitas' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
-            'indeks_keselamatan' => function(){
+            'indeks_keselamatan' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
+            'websiteViewsChart' => $websiteViewsChart,
+            'rerata_capaian_kinerja' => $rerata_capaian_kinerja
         ]);
     }
 
-    public function dashboard() {
+    public function dashboard()
+    {
         return Inertia::render('Dashboard/dashboard', [
             'periode' => \App\Models\Periode::where('status', 'Active')->first(),
             'user_count' => \App\Models\User::all()->count(),
             'indikator_count' => \App\Models\Indikator::all()->count(),
             'pic_count' => \App\Models\PIC::all()->count(),
-            'indeks_migas' => function(){
+            'indeks_migas' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
-            'indeks_aksesibilitas' => function(){
+            'indeks_aksesibilitas' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
-            'indeks_keselamatan' => function(){
+            'indeks_keselamatan' => function () {
                 $arr = [10, 30, 40, 70];
                 return $arr;
             },
--- resources/js/Pages/Dashboard/home.jsx
+++ resources/js/Pages/Dashboard/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Typography,
   Card,
@@ -25,51 +25,169 @@ import {
   projectsTableData,
   ordersOverviewData,
 } from "@/data";
-import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
+import { CheckCircleIcon, ClockIcon, PlusCircleIcon } from "@heroicons/react/24/solid";
 import NewAdminLayout from "@/layouts/NewAdminLayout";
+import { usePage } from "@inertiajs/react";
 
-export function Home({props}) {
+export function Home({ props }) {
+  console.log(usePage().props);
+  const { rerata_capaian_kinerja } = usePage().props;
+  const avgKinerja = rerata_capaian_kinerja.map(item => item.rerata_kinerja).reduce((sum, value) => sum + value, 0) / rerata_capaian_kinerja.length;
   return (
-    <NewAdminLayout props = {props}
-    children={(
-    <div className="mt-12">
-      <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-        {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
-          <StatisticsCard
-            key={title}
-            {...rest}
-            title={title}
-            icon={React.createElement(icon, {
-              className: "w-6 h-6 text-white",
-            })}
-            footer={
-              <Typography className="font-normal text-blue-gray-600">
-                <strong className={footer.color}>{footer.value}</strong>
-                &nbsp;{footer.label}
-              </Typography>
-            }
-          />
-        ))}
-      </div>
-      <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
-        {statisticsChartsData.map((props) => (
-          <StatisticsChart
-            key={props.title}
-            {...props}
-            footer={
-              <Typography
-                variant="small"
-                className="flex items-center font-normal text-blue-gray-600"
+    <NewAdminLayout props={props}
+      children={(
+        <div className="mt-12">
+          <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+            {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
+              <StatisticsCard
+                key={title}
+                {...rest}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-6 h-6 text-white",
+                })}
+                footer={
+                  <Typography className="font-normal text-blue-gray-600">
+                    <strong className={footer.color}>{footer.value}</strong>
+                    &nbsp;{footer.label}
+                  </Typography>
+                }
+              />
+            ))}
+          </div>
+          <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
+            {/*statisticsChartsData.map((props) => (
+              <StatisticsChart
+                key={props.title}
+                {...props}
+                footer={
+                  <Typography
+                    variant="small"
+                    className="flex items-center font-normal text-blue-gray-600"
+                  >
+                    <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
+                    &nbsp;{props.footer}
+                  </Typography>
+                }
+              />
+            ))*/}
+          </div>
+
+          <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
+            <Card className="border border-blue-gray-100 shadow-sm">
+              <CardHeader
+                floated={false}
+                shadow={false}
+                color="transparent"
+                className="m-0 p-6"
               >
-                <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
-                &nbsp;{props.footer}
-              </Typography>
-            }
-          />
-        ))}
-      </div>
-      
-    </div>)}></NewAdminLayout>
+                <Typography variant="h6" color="blue-gray" className="mb-2">
+                  Rerata Capaian Kinerja
+                </Typography>
+                <Typography
+                  variant="small"
+                  className="flex items-center gap-1 font-normal text-blue-gray-600"
+                >
+                  <ArrowUpIcon
+                    strokeWidth={3}
+                    className="h-3.5 w-3.5 text-green-500"
+                  />
+                  Ditjen Migas: <strong>{(parseFloat(avgKinerja)).toLocaleString(undefined, { maximumFractionDigits: 2, style: 'percent' })}</strong>
+                </Typography>
+              </CardHeader>
+              <CardBody className="pt-0">
+                {rerata_capaian_kinerja.map(
+                  ({ nama_pic, rerata_kinerja }, key) => (
+                    <div key={nama_pic} className="flex items-start gap-4 py-3">
+                      <div
+                        className={`relative p-1 after:absolute after:-bottom-6 after:left-2/4 after:w-0.5 after:-translate-x-2/4 after:bg-blue-gray-50 after:content-[''] ${key === ordersOverviewData.length - 1
+                          ? "after:h-0"
+                          : "after:h-4/6"
+                          }`}
+                      >
+                        {React.createElement(PlusCircleIcon, {
+                          className: `!w-5 !h-5 text-blue-gray-300`,
+                        })}
+                      </div>
+                      <div>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="block font-medium"
+                        >
+                          {nama_pic} :
+                        </Typography>
+
+                      </div>
+                      <div>
+                        <Typography
+                          as="span"
+                          variant="small"
+                          className="text-xs font-medium text-blue-gray-500"
+                        >
+                          {(parseFloat(rerata_kinerja)).toLocaleString(undefined, { maximumFractionDigits: 2, style: 'percent' })}
+                        </Typography>
+                      </div>
+
+                    </div>
+                  )
+                )}
+              </CardBody>
+            </Card>
+
+            <Card className="border border-blue-gray-100 shadow-sm">
+              <CardHeader
+                floated={false}
+                shadow={false}
+                color="transparent"
+                className="m-0 p-6"
+              >
+                <Typography variant="h6" color="blue-gray" className="mb-2">
+                  Capaian PK Dirjen Migas
+                </Typography>
+                <Typography
+                  variant="small"
+                  className="flex items-center gap-1 font-normal text-blue-gray-600"
+                >
+                  <ArrowUpIcon
+                    strokeWidth={3}
+                    className="h-3.5 w-3.5 text-green-500"
+                  />
+                  Ditjen Migas: <strong>15 Indikator</strong>
+                </Typography>
+              </CardHeader>
+              <CardBody className="pt-0">
+                
+                  <div  className="flex items-start gap-4 py-3">
+                    <div
+                      className="relative p-1 after:absolute after:-bottom-6 after:left-2/4 after:w-0.5 after:-translate-x-2/4 after:bg-blue-gray-50"                          
+                    >
+                      {React.createElement(PlusCircleIcon, {
+                        className: `!w-5 !h-5 text-blue-gray-300`,
+                      })}
+                    </div>
+                    <div>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="block font-medium"
+                      >
+                        
+                      </Typography>
+                      <Typography
+                        as="span"
+                        variant="small"
+                        className="text-xs font-medium text-blue-gray-500"
+                      >
+                        
+                      </Typography>
+                    </div>
+                  </div>
+               
+              </CardBody>
+            </Card>
+          </div>
+        </div>)}></NewAdminLayout>
   );
 }
 
