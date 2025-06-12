a0e92742414d75ec105dafe586104a1a24936d83
Ibnu Akil <ibnu.ial@bsi.ac.id>
2025-06-12 12:19:40 +0700

    update

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Api\BaseController;
+use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
 use App\Models\LaporanCapaian;
 use Illuminate\Http\Request;
